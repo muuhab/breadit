57073e72b79eeaa530759fd7979ad38d0080afd3
Mohab Sherif <mohab.barca@gmail.com>
2023-07-06 06:24:44 +0300

    Added feeds to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { buttonVariants } from "@/components/ui/Button";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
+import { getAuthSesssion } from "./api/auth/[...nextauth]/route";
+import GeneralFeed from "@/components/GeneralFeed";
+import CustomFeed from "@/components/CustomFeed";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSesssion()
+
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your Feed</h1>
       <div className="grid grid-cols md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/*Feed*/}
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-emerald-100 px-6 py-4">
             <p className="font-semibold py-3 flex items-center gap-1.5">
